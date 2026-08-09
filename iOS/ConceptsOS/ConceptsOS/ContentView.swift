2c4c121006dd34b39cc9095d09b96c1f1a60a917
Dan Rasmuson <danrasmuson@gmail.com>
2026-08-09 10:57:37 -0500

    iOS: point WebView at 10.10.0.1:3000

--- iOS/ConceptsOS/ConceptsOS/ContentView.swift
+++ iOS/ConceptsOS/ConceptsOS/ContentView.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 struct ContentView: View {
     // Dan's dev machine on the LAN. Change this if your IP moves.
-    private let url = URL(string: "http://192.168.1.230:3000")!
+    private let url = URL(string: "http://10.10.0.1:3000")!
 
     var body: some View {
         WebView(url: url)
